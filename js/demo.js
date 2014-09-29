83ed2e5b464f4e69a41f8a4b71f38c5bfc5538a0
Paco van der Linden <pavadeli@gmail.com>
2014-09-29 16:35:21 +0200

    First version of error handling demo's

---
+++ js/demo.js
@@ -0,0 +1,117 @@
+angular.module('demo', ['demo.errorHandler'])
+
+	// MANUAL HANDLING...
+	.factory('undecoratedService', function ($q, $timeout) {
+		return {
+			throwsAnError: function throwsAnError() {
+				throw new Error('This is an error from throwsAnError.');
+			},
+
+			promiseRejects: function promiseRejects() {
+				var defer = $q.defer();
+				$timeout(function () {
+					defer.reject('Something went wrong (asynchronously).');
+				}, 1000);
+				return defer.promise;
+			}
+		};
+	})
+	.controller('manualController', function ($scope, errorHandler, undecoratedService) {
+		$scope.manualError = function () {
+			try {
+				undecoratedService.throwsAnError();
+			} catch (err) {
+				errorHandler.errors.push(err && err.message || err);
+			}
+		};
+
+		$scope.synchronousError = function () {
+			undecoratedService.throwsAnError();
+		};
+
+		$scope.wrappedSynchronousError = function () {
+			errorHandler.call(undecoratedService.throwsAnError, undecoratedService);
+		}
+
+		$scope.asynchronousError = function () {
+			undecoratedService.promiseRejects();
+		};
+
+		$scope.manualAsynchronousError = function () {
+			undecoratedService.promiseRejects()
+				['catch'](function (err) {
+					errorHandler.errors.push(err);
+				});
+		};
+
+		$scope.wrappedAsynchronousError = function () {
+			errorHandler.call(undecoratedService.promiseRejects, undecoratedService);
+		}
+	})
+
+	// AUTOMATIC HANDLING...
+	.factory('decoratedService', function ($q, $timeout) {
+		function throwsAnError() {
+			throw new Error('You won\'t believe what just happened!');
+		}
+		throwsAnError.description = 'perform some synchronous operation';
+
+		function promiseRejectsAfterAWhile() {
+			var defer = $q.defer();
+			$timeout(function () {
+				defer.reject('Something happened, but I\'m not sure how to fix it.');
+			}, 1000);
+			return defer.promise;
+		}
+		promiseRejectsAfterAWhile.description = 'perform some asynchronous operation';
+
+		return {
+			throwsAnError: throwsAnError,
+			promiseRejectsAfterAWhile: promiseRejectsAfterAWhile
+		};
+	})
+	.config(function (errorHandlerProvider, $provide) {
+		// Decorate both our own decoratedService and the built-in $http service.
+		errorHandlerProvider.decorate($provide, ['decoratedService']);
+	})
+	.controller('autoController', function ($scope, decoratedService, $http) {
+		$scope.synchronousError = function () {
+			decoratedService.throwsAnError();
+		}
+
+		$scope.asynchronousError = function () {
+			decoratedService.promiseRejectsAfterAWhile();
+		}
+	})
+
+	// EXAMPLE SCENARIO
+	.factory('exampleService', function ($http) {
+
+		function loadData(successful) {
+			return $http.get(successful ? 'data' : 'doesntExist')
+				.then(function loadDataSuccess(result) {
+					return result.data;
+				});
+		}
+		loadData.description = 'load the example data from the \'realistic scenario\'';
+
+		return {
+			loadData: loadData
+		};
+	})
+	.config(function (errorHandlerProvider, $provide) {
+		errorHandlerProvider.decorate($provide, ['exampleService']);
+	})
+	.controller('exampleController', function ($scope, exampleService) {
+		$scope.data = '';
+		$scope.loadData = function loadData(successful) {
+			$scope.data = '';
+			exampleService.loadData(successful).then(function (data) {
+				$scope.data = data;
+			});
+		};
+	})
+
+  .run(function ($rootScope, errorHandler) {
+  	$rootScope.errorHandler = errorHandler;
+  });
---
+++ js/errorhandler.js
@@ -0,0 +1,100 @@
+'use strict';
+
+angular.module('demo.errorHandler', [])
+  .constant('httpErrors', {
+  	404: 'The requested data or service could not be found.',
+  	500: 'Unknown errors occurred at the server.'
+  })
+  .provider('errorHandler', function () {
+
+    // Wrap a single function [func] in another function that handles both synchronous and asynchonous errors.
+    function decorate($injector, obj, func) {
+      return angular.extend(function() {
+        var handler = $injector.get('errorHandler');
+        return handler.call(func, obj, arguments);
+      }, func);
+    }
+
+    return {
+      // Decorate all functions of the service [$delegate] with error handling. This function should be used as decorator
+      // function in a call to $provide.decorator().
+      decorator: ['$delegate', '$injector', function($delegate, $injector) {
+      	// Loop over all functions in $delegate and wrap these functions using the [decorate] functions above.
+        for (var prop in $delegate) {
+          if (angular.isFunction($delegate[prop])) {
+            $delegate[prop] = decorate($injector, $delegate, $delegate[prop]);
+          }
+        }
+        return $delegate;
+      }],
+
+      // Decorate the mentioned [services] with automatic error handling. See demo.js for an example.
+      decorate: function ($provide, services) {
+        var decorator = this.decorator;
+        angular.forEach(services, function (service) {
+          $provide.decorator(service, decorator);
+        });
+      },
+
+      $get: function ($log, httpErrors) {
+
+        var handler = {
+
+        	// The list of errors.
+          errors: [],
+
+          // Report the error [err] in relation to the function [func].
+          funcError: function (func, err) {
+          	if (err && err.status) {
+	          	// A lot of errors occur in relation to HTTP calls... translate these into user-friendly msgs.
+            	err = httpErrors[err.status];
+            } else if (err && err.message) {
+            	// Exceptions are unwrapped.
+            	err = err.message;
+            }
+            if (angular.isUndefined(err)) {
+              err = 'An unknown error occurred.';
+            }
+            if (func && func.description) {
+              err = 'Unable to ' + func.description + '. ' + err;
+            }
+            $log.info('Caught error: ' + err);
+            handler.errors.push(err);
+          },
+
+          // Call the provided function [func] with the provided [args] and error handling enabled.
+          call: function (func, self, args) {
+            $log.debug('Function called: ', (func.name || func));
+
+            var result;
+            try {
+              result = func.apply(self, args);
+            } catch (err) {
+              // Catch synchronous errors.
+              handler.funcError(func, err);
+              throw err;
+            }
+
+            // Catch asynchronous errors.
+            var promise = result && result.$promise || result;
+            if (promise && angular.isFunction(promise.then) && angular.isFunction(promise['catch'])) {
+            	// promise is a genuine promise, so we call [handler.async].
+              handler.async(func, promise);
+            }
+
+            return result;
+          },
+
+          // Automatically record rejections of the provided [promise] into error messages.
+          async: function (func, promise) {
+            promise['catch'](function (err) {
+              handler.funcError(func, err);
+            });
+            return promise;
+          }
+        };
+
+        return handler;
+      }
+    };
+  });
